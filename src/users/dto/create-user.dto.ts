d1ee9cb3a17ddeb1af1ab61b48ab16b89aee4082
OmarAdel898 <omar.97453@cs.mti.edu.eg>
2026-07-30 19:04:16 +0300

    feat(users): add role-based permissions for tenant_owner and warehouse_manager

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -11,6 +11,7 @@ import {
 import { UserRole } from "../entities/user.entity";
 
 export class CreateUserDto {
+  @IsOptional()
   @IsString()
   @MinLength(2)
   @MaxLength(255)
@@ -27,7 +28,7 @@ export class CreateUserDto {
   @IsString()
   @MinLength(8)
   @MaxLength(255)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/, {
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/, {
     message:
       "Password must contain uppercase, lowercase, number, and special character",
   })
