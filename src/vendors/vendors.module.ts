fb4bdd95d700b517a0080346682c25362e2ae3f0
OmarAdel898 <omar.97453@cs.mti.edu.eg>
2026-08-07 19:41:48 +0300

    feat(vendors): wire vendor catalog controller/service registration

--- src/vendors/vendors.module.ts
+++ src/vendors/vendors.module.ts
@@ -6,11 +6,14 @@ import { VendorCatalogEntry } from './entities/vendor-catalog-entry.entity';
 import { VendorsController } from './vendors.controller';
 import { VendorsService } from './vendors.service';
 import { VendorMapper } from './mappers/vendor.mapper';
+import { VendorCatalogEntriesController } from './vendor-catalog-entries.controller';
+import { VendorCatalogEntriesService } from './vendor-catalog-entries.service';
+import { VendorCatalogEntryMapper } from './mappers/vendor-catalog-entry.mapper';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Vendor, VendorCatalogEntry])],
-  controllers: [VendorsController],
-  providers: [VendorsService, VendorMapper],
-  exports: [VendorsService, VendorMapper],
+  controllers: [VendorsController, VendorCatalogEntriesController],
+  providers: [VendorsService, VendorMapper, VendorCatalogEntriesService, VendorCatalogEntryMapper],
+  exports: [VendorsService, VendorMapper, VendorCatalogEntriesService],
 })
 export class VendorsModule {}
